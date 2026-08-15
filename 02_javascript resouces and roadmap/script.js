f68cc15bd333eb6ea3bbf30784ddffe177ce18ba
waqas-khan-codecanvas <waqaskhan11411704@gmail.com>
2026-08-15 19:37:16 +0500

    js resources

---
+++ 02_javascript resouces and roadmap/script.js
@@ -0,0 +1,20 @@
+
+
+// console.log('name => ', name)
+var name = "waqas khan";
+let name = "waqas khan";
+
+
+// greet()
+
+// function greet() {
+
+//     let name = "waqas khan";
+//     console.log('hey', name)
+// }
+
+console.log(greet)
+
+var greet = ()=>{
+    console.log("waqas khan")
+}
